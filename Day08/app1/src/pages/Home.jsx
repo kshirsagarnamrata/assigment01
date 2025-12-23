353abebc0786873a158caa0eca44c135b2ad009b
Namrata Kshirsagar <kshirsagarnamrata24@gmail.com>
2025-12-23 11:49:26 +0530

    react classwork

---
+++ Day08/app1/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar";
+
+//function component
+export default function Home(){
+    return <>
+    <Navbar/>
+    <h1>Home</h1>
+    </>
+}
---
+++ Day08/app1/src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function Profile(){
+    return(
+        <div>
+            <Navbar/>
+            <div>Profile</div>
+        </div>
+    )
+}
+
+export default Profile
